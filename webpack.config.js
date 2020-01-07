bbbf5a57ec5a6541ba29375ae66d1bd5c5e891b3
Sinclair Liang <sinclair.liang@austcommunitymedia.com.au>
2020-01-07 16:54:41 +1100

    setup prod webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,51 @@
 const path = require('path')
-module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-    	module: {
-		rules: [{
-			loader: 'babel-loader',
-			test: /\.js$/,  //use REGEX to point to all the .js files
-			exclude: /node_modules/,  
-			//use REGEX to exclude the files in node_modules
-		}, {
-			test: /\.s?css$/,
-			use: [
-				'style-loader',
-				'css-loader',
-				'sass-loader'
-			]
-		}]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
+const ExtractTaxtPlugin = require('extract-text-webpack-plugin')
+
+module.exports = (env) => {
+	const isProd = env === 'production';
+	const CSSExtract = new ExtractTextPlugin('style.css');
+	console.log('env', env)
+	return {
+			entry: './src/app.js',
+			output: {
+				path: path.join(__dirname, 'public'),
+				filename: 'bundle.js'
+			},
+			module: {
+				rules: [{
+					loader: 'babel-loader',
+					test: /\.js$/,  //use REGEX to point to all the .js files
+					exclude: /node_modules/,  
+					//use REGEX to exclude the files in node_modules
+				}, {
+					test: /\.s?css$/,
+					use: [
+						CSSExtract.extract({
+							use: [
+								{
+									loader: 'css-loader',
+									options: {
+										sourceMap: true
+									}
+								},
+								{
+									loader: 'sass-loader',
+									options: {
+										sourceMap: true
+									}
+								},
+							]
+						})
+					]
+				}]
+			},
+			plugins:[
+				CSSExtract
+			],
+			devtool: isProd ? 'source-map' : 'inline-source-map',
+			devServer: {
+				contentBase: path.join(__dirname, 'public'),
+				historyApiFallback: true
+			}
 	}
 }
